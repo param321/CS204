ceb47561622231ffc8968ffe058d4ef3cbaeeba4
Param Aryan Singh <44890539+param321@users.noreply.github.com>
2019-08-04 13:58:01 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-int x,y,z,p,q,r;
+int x,y,z,p,q,r,l;
+cin>>l;
+while(l--){
 string a,b,c;
 char d;
 cin>>a>>b;
@@ -11,27 +13,28 @@ x=max(a.length(),b.length());
 y=min(a.length(),b.length());
 int rem=0;
 for(int i=0;i<x;i++){
-	if(i<y){
-		p=a[i]-'0';
-		q=b[i]-'0';
-		d=('0'+((p+q+rem)%10));
-		c=c+d;
-		rem=(p+q+rem)/10;
-	}else{
-		if(a.length()==x){
-			p=a[i]-'0';
-		}else{
-			p=b[i]-'0';
-		}
-		d=('0'+((p+rem)%10));
-		c=c+d;
-		rem=(p+rem)/10;
-	}
+    if(i<y){
+        p=a[i]-'0';
+        q=b[i]-'0';
+        d=('0'+((p+q+rem)%10));
+        c=c+d;
+        rem=(p+q+rem)/10;
+    }else{
+        if(a.length()==x){
+            p=a[i]-'0';
+        }else{
+            p=b[i]-'0';
+        }
+        d=('0'+((p+rem)%10));
+        c=c+d;
+        rem=(p+rem)/10;
+    }
 }
 if(rem==1){
-	c=c+'1';
+    c=c+'1';
 }
 reverse(c.begin(),c.end());
 cout<<c<<endl;
+}
 return 0;
 }
