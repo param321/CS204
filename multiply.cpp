4bcfa1add9f7727c6f642afb54d0015fe96a60e8
Param Aryan Singh <44890539+param321@users.noreply.github.com>
2019-08-04 14:00:12 +0530

    Update multiply.cpp

--- multiply.cpp
+++ multiply.cpp
@@ -11,31 +11,34 @@ x=max(a.length(),b.length());
 y=min(a.length(),b.length());
 int rem=0;
 for(int i=0;i<x;i++){
-	if(i<y){
-		p=a[i]-'0';
-		q=b[i]-'0';
-		d=('0'+((p+q+rem)%10));
-		c=c+d;
-		rem=(p+q+rem)/10;
-	}else{
-		if(a.length()==x){
-			p=a[i]-'0';
-		}else{
-			p=b[i]-'0';
-		}
-		d=('0'+((p+rem)%10));
-		c=c+d;
-		rem=(p+rem)/10;
-	}
+    if(i<y){
+        p=a[i]-'0';
+        q=b[i]-'0';
+        d=('0'+((p+q+rem)%10));
+        c=c+d;
+        rem=(p+q+rem)/10;
+    }else{
+        if(a.length()==x){
+            p=a[i]-'0';
+        }else{
+            p=b[i]-'0';
+        }
+        d=('0'+((p+rem)%10));
+        c=c+d;
+        rem=(p+rem)/10;
+    }
 }
 if(rem==1){
-	c=c+'1';
+    c=c+'1';
 }
 reverse(c.begin(),c.end());
 return c;
 }
 
 int main(){
+int l;
+cin>>l;
+while(l--){
 int x,y,z,p,q,r=0;
 string a,b,c,e,f;
 char d;
@@ -45,43 +48,44 @@ reverse(b.begin(),b.end());
 x=max(a.length(),b.length());
 y=min(a.length(),b.length());
 if(b.length()==x){
-	c=b;
-	b=a;
-	a=c;
+    c=b;
+    b=a;
+    a=c;
 }
 int rem;
 for(int i=0;i<y;i++){
-	c="";
-	p=b[i]-'0';
-	rem=0;
-	for(int j=0;j<x;j++){
-		q=a[j]-'0';
-		d='0'+(p*q+rem)%10;
-		c=c+d;
-		rem=(p*q+rem)/10;
-	}
-	d='0'+rem;
-	c=c+d;
-	reverse(c.begin(),c.end());
-	for(int j=0;j<i;j++){
-		c=c+'0';
-	}
-	e=add(e,c);
+    c="";
+    p=b[i]-'0';
+    rem=0;
+    for(int j=0;j<x;j++){
+        q=a[j]-'0';
+        d='0'+(p*q+rem)%10;
+        c=c+d;
+        rem=(p*q+rem)/10;
+    }
+    d='0'+rem;
+    c=c+d;
+    reverse(c.begin(),c.end());
+    for(int j=0;j<i;j++){
+        c=c+'0';
+    }
+    e=add(e,c);
 }
 for(int i=0;i<e.length();i++){
-	if(e[i]=='0'){
-		r++;
-	}else{
-		break;
-	}
+    if(e[i]=='0'){
+        r++;
+    }else{
+        break;
+    }
 }
 for(int i=r;i<e.length();i++){
-	f=f+e[i];
+    f=f+e[i];
 }
 if(f.empty()){
-	cout<<0<<endl;
+    cout<<0<<endl;
 }else{
-	cout<<f<<endl;
+    cout<<f<<endl;
+}
 }
 return 0;
 }
