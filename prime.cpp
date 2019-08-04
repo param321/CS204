b4f2a02be2948dc061e79ee18612cd85e90ee1a3
Param Aryan Singh <44890539+param321@users.noreply.github.com>
2019-08-04 13:49:45 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 
 int main(){
-unsigned long long x,y,z,p,q,r,i;
+unsigned long long x,y,z,p,q,r,i,l;
 string a,b,c,e;
-cin>>x;
-r=1;
-for(i=2;i<=sqrt(x);i++){
-    if(x%i==0){
-        r=0;
-        break;
+cin>>l;
+while(l--){
+    cin>>x;
+    r=1;
+    for(i=2;i<=sqrt(x);i++){
+        if(x%i==0){
+            r=0;
+            break;
+        }
     }
-}
-if(r==1){
-    cout<<"PRIME"<<endl;
-}else{
-    cout<<"NOT PRIME"<<endl;
+    if(r==1){
+        cout<<"Prime"<<endl;
+    }else{
+        cout<<"Not a Prime"<<endl;
+    }
 }
 return 0;
 }
