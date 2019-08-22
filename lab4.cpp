a6adcbe5907a62d074acde757bb4e1b88522ae5d
Param Aryan Singh <44890539+param321@users.noreply.github.com>
2019-08-22 20:51:25 +0530

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,188 +1,203 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-struct tree{
-	string x;
-	tree *left;
-	tree *right;
-};
-
-bool isOperator(char c){ 
-    if(c=='+'||c=='-'||c=='*'||c=='/'||c=='^'){
-        return true; 
-    }else{
-    	return false;
-    } 
-} 
-
-tree *newNode(int v){ 
-    tree *temp = new tree; 
-    temp->left = temp->right = NULL; 
-    temp->x = v; 
-    return temp; 
-}; 
-
-int pr(char c){
-	if(c=='^'){
-		return 5;
-	}else if(c=='/'){
-		return 4;
-	}else if(c=='*'){
-		return 4;
-	}else if(c=='+'){
-		return 1;
-	}else if(c=='-'){
-		return 1;
-	}else{
-		return -1;
-	}
+int isoperator(string c)
+{
+    if (c == "^" || c == "/" || c == "*" || c == "+" || c == "-"||c=="(")
+        return 1;
+    return 0;
+}
+int inpre(string c)
+{
+    if (c == "^")
+        return 5;
+    if (c == "*" || c == "/")
+        return 4;
+    if (c == "+" || c == "-")
+        return 2;
+    else 
+        return 1;
 }
 
-string postfi(string a){
-	stack <char> st; 
-    st.push('N'); 
-    int l = a.length(); 
-    string ns; 
-    for(int i = 0; i < l; i++){
-    	//if(a[i]<'0'||a[i]>'9'){
-    		//ns=ns+" ";
-    	//}
-        // If the scanned character is an operand, add it to output string. 
-        if((a[i]>='0'&&a[i]<='9')){
-        	ns+=a[i];
-        // If the scanned character is an ‘(‘, push it to the stack. 
-        }else if(a[i]=='('){
-        	st.push('('); 
-        // If the scanned character is an ‘)’, pop and to output string from the stack 
-        // until an ‘(‘ is encountered. 
-        }else if(a[i] == ')'){
-            while(st.top() != 'N' && st.top() != '('){ 
-                char c = st.top(); 
-                st.pop(); 
-               	ns += c; 
-            } 
-            if(st.top() == '('){ 
-                char c = st.top(); 
-                st.pop(); 
-            } 
-        }else{ 
-            while(st.top() != 'N' && pr(a[i]) <= pr(st.top())) 
-            { 
-                char c = st.top(); 
-                st.pop(); 
-                ns += c; 
-            } 
-            st.push(a[i]); 
-        } 
-  
-    } 
-    //Pop all the remaining elements from the stack 
-    while(st.top() != 'N') 
-    { 
-        char c = st.top(); 
-        st.pop(); 
-        ns += c; 
-    }
-	return ns;
-}
-
-tree* constructTree(string postfix){ 
-    stack<tree *> st; 
-    tree *t, *t1, *t2; 
-    // Traverse through every character of 
-    // input expression 
-    for (int i=0; i<postfix.length(); i++){ 
-        // If operand, simply push into stack 
-        if (!isOperator(postfix[i])){ 
-            t = newNode(postfix[i]); 
-            st.push(t); 
-        }else{ 
-            t = newNode(postfix[i]);
-            // Pop two top nodes 
-            t1 = st.top(); // Store top 
-            st.pop();      // Remove top 
-            t2 = st.top(); 
-            st.pop();
-            //  make them children 
-            t->right = t1; 
-            t->left = t2;
-            // Add this subexpression to stack 
-            st.push(t); 
-        } 
-    } 
-    //  only element will be root of expression 
-    // tree 
-    t = st.top(); 
-    st.pop(); 
-    return t; 
-}
-
-void inorder(tree *t) { 
-    if(t){ 
-        inorder(t->left); 
-        cout<<(t->x)<<" ";
-        inorder(t->right); 
-    } 
-} 
-
-int toInt(string s)  {  
-    int num = 0; 
-    // Check if the integral value is  
-    // negative or not 
-    // If it is not negative, generate the number  
-    // normally 
-    if(s[0]!='-') 
-        for (int i=0; i<s.length(); i++)  
-            num = num*10 + (int(s[i])-48);  
-    // If it is negative, calculate the +ve number 
-    // first ignoring the sign and invert the  
-    // sign at the end 
+int outpre(string c)
+{
+    if (c == "^")
+        return 6;
+    if (c == "*" || c == "/")
+        return 3;
+    if (c == "+" || c == "-")
+        return 1;
     else
-        for(int i=1;i<s.length();i++){ 
-            num = num*10 + (int(s[i])-48);  
-            num = num*-1; 
-        } 
-    return num;  
-}  
+        return -1;
+    
+}
+vector<string> stov(string s)
+{
+    int n = s.size();
+    vector<string> v;
+    for (int i = 0; i < n; i++)
+    {
+        string temp = "";
+        if ((s[i] >= '0' && s[i] <= '9'))
+        {
+            int j = i;
+            while (i != s.size() && s[i] >= '0' && s[i] <= '9')
+            {
+                temp += s[i];
+                i++;
+            }
+            i--;
+        }
+        else
+        {
+            temp += s[i];
+        }
+        if (temp=="-")
+        {
+            if (v.empty())
+                v.push_back("0");
+            else
+            {
+                if (isoperator(v.back()))
+                    v.push_back("0");
+            }
+        }
+        v.push_back(temp);
+    }
+    return v;
+}
+vector<string> post(vector<string> s)
+{
+    stack<string> st;
+    st.push("E");
+    int l = s.size();
+    vector<string> nv;
+    for (int i = 0; i < l; i++)
+    {
+        if ((s[i][0] >= '0' && s[i][0] <= '9'))
+            nv.push_back(s[i]);
+        else if (s[i] == "(")
+            st.push("(");
+        else if (s[i] == ")")
+        {
+            while (st.top() != "E" && st.top() != "(")
+            {
+                string c = st.top();
+                st.pop();
+                nv.push_back(c);
+            }
+            if (st.top() == "(")
+            {
+                string c = st.top();
+                st.pop();
+            }
+        }
+        else
+        {
+            while (st.top() != "E" && outpre(s[i]) < inpre(st.top()))
+            {
+                string c = st.top();
+                st.pop();
+                nv.push_back(c);
+            }
+            st.push(s[i]);
+        }
+    }
+    while (st.top() != "E")
+    {
+        string c = st.top();
+        st.pop();
+        nv.push_back(c);
+    }
 
-int eval(struct tree* root){  
-    // empty tree  
-    if (!root)  
-        return 0;  
-    // leaf node i.e, an integer  
-    if (!root->left && !root->right)  
-        return toInt(root->x);  
-    // Evaluate left subtree  
-    int l_val = eval(root->left);  
-    // Evaluate right subtree  
-    int r_val = eval(root->right);  
-    // Check which operator to apply  
-    if (root->x=="+")  
-        return l_val+r_val; 
-    if (root->x=="^")  
-        return pow(r_val,l_val); 
-    if (root->x=="-")  
-        return l_val-r_val;  
-    if (root->x=="*")  
-        return l_val*r_val;  
-    return l_val/r_val;  
-}  
+    return nv;
+}
+struct node
+{
+    string s;
+    node *lch = NULL, *rch = NULL;
+};
+node *ctree(vector<string> str)
+{
 
+    node *root = NULL;
+    vector<node *> stv;
+    int i = 0;
+    while (i != str.size())
+    {
+        if (str[i] != "+" && str[i] != "-" && str[i] != "*" && str[i] != "/" && str[i] != "^")
+        {
+            node *temp = (node *)malloc(sizeof(node));
+            temp->s = str[i];
+            for(int j=0;j<str[i].size();j++)
+            {
+                if (str[i][j] < '0' || str[i][j] > '9')
+                    return NULL;
+            }
+            stv.push_back(temp);
+        }
+        else
+        {
+            if (stv.size()< 2)
+                return NULL;
+            node *s1 = stv.back();
+            stv.pop_back();
+            node *s2 = stv.back();
+            stv.pop_back();
+            node *temp = (node *)malloc(sizeof(node));
+            temp->s = str[i];
+            temp->lch = s2;
+            temp->rch = s1;
+            stv.push_back(temp);
+        }
+        i++;
+    }
+    if (stv.size()!= 1)
+        return NULL;
+    return stv.back();
+}
+int value(node *root)
+{
+    string s = root->s;
+    int ans = 0;
+    if (s != "+" && s != "-" && s != "*" && s != "/" && s != "^")
+        return stoi(s);
+    else
+    {
+        int lans = value(root->lch), rans = value(root->rch);
+        if (s == "+")
+            ans = lans + rans;
+        else if (s == "-")
+            ans = lans - rans;
+        else if (s == "*")
+            ans = lans * rans;
+        else if (s == "/")
+            ans = lans / rans;
+        else
+        {
+            ans = 1;
+            for(int i=0;i<rans;i++) ans *= lans;
+        }
+    }
+    return ans;
+}
 int main(){
-	string a,b,c;
-	int x,y,z,p,q;
-	cin>>x;
-	for(int i=0;i<x;i++){
-		cin>>y;
-		for(int j=0;j<y;j++){
-			cin>>a;
-			b=postfi(a);
-			cout<<b<<endl;
-			tree* r = constructTree(b);
-			inorder(r);
-			cout<<endl;
-			cout << eval(r)<<endl;
-		}
-	}
-	return 0;
+    int t, n;
+    cin >> t;
+    while (t--){
+        cin >> n;
+        while (n--)
+        {
+            string s;
+            cin >> s;
+            vector<string> str = stov(s);
+            str = post(str);
+            node *root = ctree(str);
+            if (root == NULL)
+                cout << "CANT BE EVALUATED" << endl;
+            else
+                cout << value(root) << endl;
+        }
+    }
+    return 0;
 }
